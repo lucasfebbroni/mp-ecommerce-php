946a27ea6dc309d7c9de5383602b627d8800b229
Lucas Febbroni <pushuno@MacBook-Pro-de-Lucas.local>
2020-08-24 00:07:08 -0300

    Cambio en notificacion webhook

--- notify.php
+++ notify.php
@@ -4,13 +4,10 @@ $topic = $_GET["topic"];
 
 $data = file_get_contents('php://input');
 
-file_get_contents('https://www.hotelpedraza.com/includes/data.php?data='.$data); 
 switch($topic) {
     case "payment":
         $data = file_get_contents('https://api.mercadopago.com/v1/payments/'.$id.'?access_token=APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
         echo $data;
-        /*$products = json_decode($data, false);
-        var_dump($products);*/
         
         http_response_code(200);
     break;
